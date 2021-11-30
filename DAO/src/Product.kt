1e5d17a2cfc63c5d6cb6a06c90e2b6df12e184cf
Werwolf2085 <70657064+Werwolf2085@users.noreply.github.com>
2021-11-30 22:48:58 +0300

    Add files via upload

---
+++ DAO/src/Product.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+abstract class Products
+@Serializable
+data class Apple(private val nameProd: String = "Apple", private val info: String="apple very good"): Products()
+@Serializable 
+data class Banana(private val name: String ="Banana", private val info: String="banana very old"): Products()
+
+
+interface Dao{
+
+    fun readProduct()
+    fun writeProduct(prod: Products)
+}
+  class DaoProducts: Dao {
+     private val path="text.txt"
+     private val list= mutableListOf<Products>()
+     override fun readProduct()  {
+         File("text.txt").useLines { lines -> lines.forEach { list.add(it) }}
+         list.forEach { println(">  " + it) }
+     }
+     override fun writeProduct(prod: Products) {
+         list.add(prod)
+         JSON.parse(DaoProducts.serializer(), list,"\n")
+         File(path).WriteText(list)
+     }
+
+ }
